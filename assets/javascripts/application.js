a3168061f914b09305b77cdf40fe34a08566f4ad
Magno Mathias <magnomathias21@gmail.com>
2014-04-24 12:12:19 -0300

    commiting last images changes

---
+++ assets/javascripts/application.js
@@ -0,0 +1,12 @@
+$("#container").fitVids();
+
+//  FitText's formula:
+//
+//  fontSize = elementWidth / (compressor * 10)
+//  fontSize = 42
+//  elementWidth = 1000
+//  compressor = 100/42 = 2.380952381
+// $("#container").find(".section-header-title").fitText(2.380952381, {
+//   minFontSize: "36px",
+//   maxFontSize: "72px"
+// });
